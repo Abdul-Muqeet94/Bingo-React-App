7910de1b039bd6bb8a8539e836138186b4503c2d
Abdul Muqeet <abdul.muqeet@uni-rostock.de>
2021-12-08 15:42:16 +0100

    bingo app

---
+++ README.md
@@ -0,0 +1,15 @@
+![Bingo1](https://user-images.githubusercontent.com/14060649/112765107-ed95f580-900b-11eb-9adf-0522d0f23820.jpg)
+![Bingo2](https://user-images.githubusercontent.com/14060649/112765111-f25aa980-900b-11eb-9fc1-3130e52d7884.jpg)
+# BINGO GAME
+
+Simple Pandemic BINGO GAME built on React JS. 
+
+Confetti used to celebrate BINGO.
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
---
+++ src/index.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import { start } from "./Confetti";
+import shuffle from "shuffle-array";
+
+
+
+const that=this;
+function Confetti() {
+
+  
+  useEffect(() => {
+      start();
+  });
+  return <canvas id="canvas" />;
+}
+
+function Tile({ id, children, onToggle, isSet }) {
+  return (
+    <div onClick={onToggle} className={`tile ${isSet ? "tile--set" : ""}`}>
+      {children}
+    </div>
+  );
+}
+
+
+const bingo = [
+  "Took a walking meeting via phone",
+  "Can you hear me?",
+  "Joked about how long your 'commute is",
+  "Said 'lol' and meant it",
+  "Had a meeting interrupted by a pet or family member",
+  "Wore pajama bottoms to a video meeting",
+  "Spotted a pet in background on a meeting",
+  "Did household chores on your lunch break",
+  "Drank a glass of water",
+  "Took a quick stroll to stretch your legs",
+  "Sent a gif to a co-worker",
+  "Took a coffee break.. or three",
+  "Did eye strain relief exercises",
+  "Sorry i was on mute",
+  "Can everyone see my screen?",
+  "Set up working hours",
+  "Used an emoji in an email",
+  "Installed a blue light blocker on your computer",
+  "Created a makeshift (standing desk)",
+  "Tried the pomodoro technique",
+  "Did chair yoga",
+  "Worked from your balcony",
+  "Took a typing speed test for fun",
+  "Created a workday playlist",
+  "Something extra"
+];
+
+
+const data= create_data()
+function create_data(){
+    var result = shuffle(bingo).reduce(
+    (data, value, index) => ({ ...data, [index]: value }),
+  {});
+  result[12]="Covid-19 Pandemic"
+  return result;
+}
+
+function refreshPage() {
+  console.log(data)
+  window.location.reload(false);
+} 
+
+function App() {
+  const [state, setState] = useState({ checked: {12:true} });
+  
+  const isWon = checked => {
+    const range = [0, 1, 2, 3, 4];
+    return (
+      undefined !==
+        range.find(row => range.every(column => checked[row * 5 + column])) ||
+      undefined !==
+        range.find(column => range.every(row => checked[row * 5 + column])) ||
+      range.every(index => checked[index * 5 + index]) ||
+      range.every(index => checked[index * 5 + 4 - index])
+    );
+  };
+  const toggle = id => {
+    if(id !=12){
+      setState(state => {
+        const checked = { ...state.checked, [id]: !state.checked[id] };
+        console.log(checked)
+
+        const won = isWon(checked);
+        
+        return {
+          ...state,
+          checked,
+          won
+        };
+    });
+      }
+ 
+    };
+
+  return (
+    <div className="App">
+      <div>
+      <h1> Bingo Game </h1>
+
+      </div>
+      <div className="wrapper">
+        {Object.keys(data).map(id => (
+          <Tile
+            key={id}
+            id={id}
+            isSet={!!state.checked[id]}
+            onToggle={() => toggle(id)}
+          >
+            {data[id]}
+          </Tile>
+        ))}
+        <button  onClick={  refreshPage }>
+      Reload Game
+      </button>
+      </div>
+      {state.won ? <Confetti /> : null}
+      
+    </div>
+    
+  );
+}
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
